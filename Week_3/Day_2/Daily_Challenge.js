071399c1de5e96cad371e1b7b98315f792b2f855
Dansultan <d.sultan147@gmail.com>
2021-07-20 13:01:18 +0300

    Week 3 Day 2

---
+++ Week_3/Day_2/Daily_Challenge.js
@@ -0,0 +1,33 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+fruits.shift();
+
+console.log(fruits);
+
+
+fruits.sort();
+
+console.log(fruits);
+
+
+fruits.push("Kiwi");
+
+console.log(fruits)
+
+
+fruits.splice(0,1);
+
+console.log(fruits);
+
+
+fruits.reverse();
+
+console.log(fruits)
+
+
+//Exercice 2 
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0])
---
+++ Week_3/Day_2/XP_GOLD.js
@@ -0,0 +1,23 @@
+// Favorite Color
+
+let me = ["my","favorite","color","is","blue"]
+let joinwords = me.toString()
+console.log(joinword)
+
+// Mixtape
+first = "marco"
+second = "polo"
+slice_1 = first.slice(2)
+slice_2 = second.slice(2)
+third = slice_1 + slice_2
+console.log(third)
+
+
+// Calculator
+let str1 = prompt("Please enter your first number :");
+let num1 = parseInt(str1)
+let str2 = prompt("Please enter your second number :");
+let num2 = parseInt(str2)
+let sum = num1 + num2
+console.log(alert(sum))
+
