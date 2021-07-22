11ac9ec6521021501928b041d28117f8211c25d9
Dansultan <d.sultan147@gmail.com>
2021-07-22 19:21:39 +0300

    W3D4

---
+++ Week_3/Day_1/nemo.js
@@ -0,0 +1,11 @@
+
+/
+let nemo = prompt("Please enter a sentence with the word 'nemo':")
+
+if (nemo.includes('nemo')){
+
+  console.log('I found nemo')}
+else { 
+  
+console.log("Sorry, couldn't find nemo")}
+
