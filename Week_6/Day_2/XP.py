de86bdcb96b61ade69ea5e2466c0948c1ec7f2bf
Dansultan <d.sultan147@gmail.com>
2021-08-12 12:03:28 +0200

    W6D2 XP

---
+++ Week_6/Day_2/XP.py
@@ -0,0 +1,55 @@
+#Exercice 1
+print ("Hello World "*4)
+
+#Exercice 2
+a = 99**3
+b = 8
+print(a*b)
+
+#Exercice 3
+False
+True
+False
+False
+False
+
+#Exercice 4
+computer_brand = 'mac'
+
+#Exercice 5
+name = 'Dan'
+age = '30'
+shoe_size = '10'
+info = "My name is "+name+" I'm "+age+" and if you wanna buy me a gift, my "+shoe_size
+print(info)
+
+#Exercice 6
+a = 16
+b = 14
+if a>b:
+    print("Hello World")
+
+#Exercice 7
+a = int(input("Please enter a number :"))
+if a%2==0:
+    print("Even")
+else:
+    print('Odd')
+
+#Exercice 8
+
+a = input('Please enter your name :')
+b = 'Dan'
+if a==b:
+    print('Ooohhh WTF')
+
+#Exercice 9
+
+a = int(input('Enter your height in inches :'))
+b = 145*0.393701
+if a>b:
+    print('You can ride')
+else :
+    print('Go home')
+
+
